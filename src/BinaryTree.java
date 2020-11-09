be3deb519042e3a089f4a010104c3243c4048d59
sasha (that's GitHub::TheBigSasha, or Alexander Aleshchenko 260923564) <alexander.aleshchenko@mail.mcgill.ca>
2020-11-09 00:28:17 -0500

    Add javadoc && GitIgnore

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,6 +1,13 @@
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * This data structures adds stuff to it in a way that keeps it in order,
+ * or rather easy to get into order quickly. Good for homies with chaotic
+ * energy who may want to spontaneously add and remove items from their
+ * dataset but still need to be able to quickly get their s*** together
+ * in case someone wants things in order.
+ */
 public class BinaryTree<E extends Comparable> implements Iterable<E> {
     private TreeNode<E> root;
 
@@ -8,22 +15,40 @@ public class BinaryTree<E extends Comparable> implements Iterable<E> {
 
     }
 
+    /**
+     * Adds an item to the tree, it puts it in its place right away
+     * @param data what is being added
+     */
     public void add(E data){
 
     }
 
+    /**
+     * We return the contents of the tree in sort order!
+     * @return a list of the items in the tree in order
+     */
     public List<E> inOrder(){
         return null;
     }
 
+    /**
+     * We return the contents of the tree in reverse sort order!
+     * @return a list of the items in the tree in reverse order
+     */
     public List<E> reverseOrder(){
         return null;
     }
+
 
     private List<E> inOrder(TreeNode<E> root, List<E> list){
         return null;
     }
 
+    /**
+     * Shh! We are being sneaky with our recursion and hiding it.
+     * This is helping us with reverseOrder()
+     * @return a list of the items in the tree in reverse order
+     */
     private List<E> reverseOrder(TreeNode<E> root, List<E> list){
         return null;
     }
