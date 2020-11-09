4b5677df670df80dfc20072e985c338aae8f0362
thebigsasha <sashathethird@gmail.com>
2020-11-09 14:02:56 -0500

    Monday_Solutions

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * This data structures adds stuff to it in a way that keeps it in order,
@@ -31,28 +32,65 @@ public class BinaryTree<E extends Comparable<E>> implements Iterable<E> {
      * @param data what is being added
      */
     public void add(E data){
+        //Take a data, compare it to what is in the root,
+        //If it is smaller, put it on one side recursively, else
+        //the other side. If next is null, that's the spot.
+        if(root == null) {
+        root = new TreeNode<>(data);    size++;
+        }else{
+            add(data, root);
+        }
+    }
+
+    private void add(E data, TreeNode<E> node){
+        if(!(data.compareTo(node.getData()) < 0)){
+            if(node.getLeft() == null){
+                node.setLeft(new TreeNode<>(data));size++;
+            }else{
+                add(data,node.getLeft());
+            }
+        } else{
+            if(node.getRight() == null){
+                node.setRight(new TreeNode<>(data));size++;
+            }else{
+                add(data,node.getRight());
+            }
+        }
 
     }
+
+
 
     /**
      * We return the contents of the tree in sort order!
      * @return a list of the items in the tree in order
      */
-    public List<E> inOrder(){
-        return null;
+    public List<E> reverseOrder(){
+        Stack<E> output = new Stack<>();
+        return  reverseOrder(root,output);
     }
 
     /**
      * We return the contents of the tree in reverse sort order!
      * @return a list of the items in the tree in reverse order
      */
-    public List<E> reverseOrder(){
-        return null;
+    public Stack<E> inOrder(){
+        Stack<E> output = new Stack<>();
+        return  inOrder(root,output);
     }
 
 
-    private List<E> inOrder(TreeNode<E> root, List<E> list){
-        return null;
+    private List<E> reverseOrder(TreeNode<E> temp, List<E> list){
+        if(temp.getLeft() != null){
+            reverseOrder(temp.getLeft(),list);
+        }
+
+        list.add(temp.getData());
+
+        if(temp.getRight() != null){
+            reverseOrder(temp.getRight(),list);
+        }
+        return list;
     }
 
     /**
@@ -60,8 +98,18 @@ public class BinaryTree<E extends Comparable<E>> implements Iterable<E> {
      * This is helping us with reverseOrder()
      * @return a list of the items in the tree in reverse order
      */
-    private List<E> reverseOrder(TreeNode<E> root, List<E> list){
-        return null;
+    private Stack<E> inOrder(TreeNode<E> temp, Stack<E> list){
+        if(temp.getRight() != null){
+            inOrder(temp.getRight(),list);
+        }
+
+        list.add(temp.getData());
+        System.out.println(temp.getData());
+
+        if(temp.getLeft() != null){
+            inOrder(temp.getLeft(),list);
+        }
+        return list;
     }
 
     /**
@@ -71,18 +119,17 @@ public class BinaryTree<E extends Comparable<E>> implements Iterable<E> {
      */
     @Override
     public Iterator<E> iterator() {
+        Stack<E> inOrder = this.inOrder();
         return new Iterator<E>() {
+
             @Override
             public boolean hasNext() {
-
-                //TODO: This
-                return false;
+                return inOrder.isEmpty();
             }
 
             @Override
             public E next() {
-                //TODO: This
-                return null;
+                return inOrder.pop();
             }
         };
     }
@@ -130,16 +177,18 @@ public class BinaryTree<E extends Comparable<E>> implements Iterable<E> {
     private static void testBinaryTree(){
         BinaryTree<PocketMonster> tree = new BinaryTree<>();
         ArrayList<PocketMonster> list = new ArrayList<>();
-        for(int i = 0; i < 150; i++){
+        System.out.println("We are running this crap!");
+        for(int i = 0; i < 20; i++){
             PocketMonster pm = PocketMonster.createPocketMonster();
             tree.add(pm);
             list.add(pm);
         }
-        list.sort(PocketMonster::compareTo());
+        list.sort(PocketMonster::compareTo);
         List<PocketMonster> treeTraversed = tree.inOrder();
         if(treeTraversed.size() != list.size()) System.out.println("FAILED! Sizes do not match!");
         for(int i = 0; i < list.size(); i++){
             if(!treeTraversed.get(i).equals(list.get(i))) System.out.println("FAILED! Not in sort order at index " + i);
+            System.out.println(treeTraversed.get(i) + " " + list.get(i));
         }
     }
 
